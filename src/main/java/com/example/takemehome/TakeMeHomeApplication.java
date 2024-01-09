170ac0ddd67fd7f58012d45731155ab8bbea746b
JGoo99 <zzgg3040@naver.com>
2024-01-09 21:05:43 +0900

    project settings

---
+++ src/main/java/com/example/takemehome/TakeMeHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.takemehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeMeHomeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TakeMeHomeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/takemehome/TakeMeHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takemehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeMeHomeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
